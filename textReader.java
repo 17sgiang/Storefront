66ca31ee5fbd880491c6bfe3743a31eb29f8a437
Jaykom <72894558+Jaykom@users.noreply.github.com>
2020-10-17 22:59:09 -0400

    Add files via upload

--- textReader.java
+++ textReader.java
@@ -5,7 +5,7 @@ class textReader extends JFrame
 {
   public textReader() throws IOException
   {
-	  File storeList = new File("A.txt");
+	    File storeList = new File("A.txt");//Here is where you put the txt file you write to when the python portion executes
 	    FileReader fr = new FileReader(storeList);
 	    BufferedReader br = new BufferedReader(fr);
 
@@ -15,7 +15,7 @@ class textReader extends JFrame
 	    while ((line = br.readLine()) != null) {
 	      row.add(line);
 	    }
-	    JOptionPane.showMessageDialog(null, new JScrollPane(new JList(row)));
+	    JOptionPane.showMessageDialog(null, new JList(row));
 	    fr.close();
  
 }
--- userGui.java
+++ userGui.java
@@ -1,3 +1,5 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -5,9 +7,11 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.file.WatchKey;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 // Using Frame class in package java.awt
 import javax.swing.*;
@@ -20,17 +24,18 @@ public class userGui implements ActionListener {
 	private static JTextField lat;
 	private static JTextField notLat;
 	private static JButton button;
-	public userGui()
-    {
-	  
-      }
+
     
 	
     public static void main(String args[]) throws IOException{
      
     	JPanel panel = new JPanel();
         JFrame frame = new JFrame();
-        frame.setSize(300, 200);
+        frame.setSize(200, 200);
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) ((dimension.getWidth() - frame.getWidth()) / 2);
+        int y = (int) ((dimension.getHeight() - frame.getHeight()) / 2);
+        frame.setLocation(x, y);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.add(panel);
@@ -38,23 +43,23 @@ public class userGui implements ActionListener {
         panel.setLayout(null);
         
         label = new JLabel("Radius");
-        label.setBounds(10, 20, 40, 25);
+        label.setBounds(25, 20, 40, 25);
         panel.add(label);
         
         radius = new JTextField(20);
         radius.setBounds(100, 20, 75, 25);
         panel.add(radius);
         
-        label2 = new JLabel("Lat");
-        label2.setBounds(10, 50, 80, 25);
+        label2 = new JLabel("Latitude");
+        label2.setBounds(25, 50, 80, 25);
         panel.add(label2);
         
         lat = new JTextField(20);
         lat.setBounds(100, 50, 75, 25);
         panel.add(lat);
         
-        label3 = new JLabel("Long");
-        label3.setBounds(10, 80, 80, 25);
+        label3 = new JLabel("Longitude");
+        label3.setBounds(25, 80, 80, 25);
         panel.add(label3);
         
         notLat = new JTextField(20);
@@ -62,7 +67,7 @@ public class userGui implements ActionListener {
         panel.add(notLat);
         
         button = new JButton("Search!");
-        button.setBounds(10, 120, 150, 25);
+        button.setBounds(25, 125, 150, 20);
         button.addActionListener(new userGui());
         panel.add(button);
         
@@ -73,34 +78,28 @@ public class userGui implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e){
 		
+		
 		String radi = radius.getText();
     	String latt = lat.getText();
     	String notLatt = notLat.getText();
     	
     	try {
 			PrintStream o = new PrintStream(new File("A.txt"));
-			PrintStream console = System.out;
 			
 			System.setOut(o);
 			System.out.println(radi); 
 			System.out.println(latt);  
-			System.out.println( notLatt);
-		        
-		    new textReader().setVisible(true);
+			System.out.println(notLatt);
+			
+			Thread.sleep(2000);
+			new textReader().setVisible(true);
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
+		
 			e1.printStackTrace();
 			
-			TimerTask task = new fileWatcher( new File("A.txt") ) {
-			      protected void onChange( File file ) {
-			        // here we code the action on a change
-			        System.out.println( "File "+ file.getName() +" have change !" );
-			      }
-			    };
-
-			    Timer timer = new Timer();
-			    // repeat the check every second
-			    timer.schedule( task , new Date(), 1000 );
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
     
     	
