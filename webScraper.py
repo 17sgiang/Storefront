fe07684c55abff11931828b2f2ac7e3b4ae5830c
Steven Giang <43970321+fifteenbongos@users.noreply.github.com>
2020-10-17 20:59:44 -0400

    Add files via upload
    Changes web scraper to deal with a list of words and dictionary of websites. Might not be correctly formatted, check when other files are finished.

--- webScraper.py
+++ webScraper.py
@@ -1,8 +1,23 @@
 """
 
 Created on Sat Oct 17
-Takes in a target word to scrape for and a website's URL to scrape.
-Returns
+
+Parameters:
+    list of words (to scrape for)
+    list of dictionaries. Each dictionary is a site
+    Site items:
+        "name": common name for the location
+        "website": url for the corresponding site
+
+Returns:
+    list of dictionaries, where each dictionary is a site.
+    This list is a filtered version of the parameter list. 
+    
+Potentially an issue of the search range being too broad.
+Currently, if the scraper finds one instance of a key word in the html, the entire
+site is added, which might result in too many sites being added.
+Uses BeautifulSoup library.
+
 @author: steveng
 
 """
@@ -12,29 +27,45 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import re
 
-# Class def
-
-# Use BeautifulSoup library to scrape a web page for a target word
-# Later I'll decide whether the return value is true/false or hit count
-# Currently returns the number of times a url contains the target word
-def webScraper(word, targetUrl):
+"""
+    return type is a list of dictionaries
+    [
+     {name:"",website:""}
+     {name:"",website:""}
+     ]        
     
-    print("Searching for '" + word + "' at url: " + targetUrl)
-    page = urlopen(targetUrl)
-    html = page.read().decode("utf-8")
-    soup = BeautifulSoup(html, "html.parser")
-    # print(soup.get_text())
-    # print(soup.find_all(string=re.compile(word)))
-    matchList = soup.find_all(string=re.compile(word))
-    hitCount = 0
     
-    for i in matchList:
-        hitCount += 1
-        
-    return hitCount    
+"""
 
-# Test case
-print("hitCount: " + str(webScraper("RegEx", "http://w3schools.com/python/python_regex.asp")))
+# Takes a list of dictionaries where names are coupled with website URLs
+# Returns a filtered version of that based on searched keywords
+def webScraper(words, websiteList):
+    returnList = []
+    
+    # websiteList is a list of dictionaries
+    # siteDict should be a dictionary
+    for siteDict in websiteList:
+        # isFound = 0
+        # Check if the key word appears in the html of a website
+        for word in words:
+            print("Searching for '" + word + "' at url: " + siteDict["website"])
+            page = urlopen(siteDict["website"])
+            
+            html = page.read().decode("utf-8")
+            soup = BeautifulSoup(html, "html.parser")
+            # print(soup.get_text())
+            # print(soup.find_all(string=re.compile(word)))
+            matchList = soup.find_all(string=re.compile(word))
+            
+            # If there's at least one match for the words, add the website and move to the next
+            if(len(matchList) > 0): 
+                # found = 1
+                obj = {"name":siteDict["name"],"website":siteDict["website"]}
+                returnList.append(obj)
+                break
+            
+    return returnList
+
 
     
 
